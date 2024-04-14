69a8e7d53a02458921dd10ed871cd4c6140ca7cd
Lbrooks13 <lbrooks1308@gmail.com>
2024-04-14 13:59:29 +0100

    Secret room 2 added

--- run.py
+++ run.py
@@ -1,31 +1,151 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+# Write your code to expect a terminal of 80 characters wide and 24 rows high
+userInput = input()
+validOption = "Please Enter A Valid Option"
+Secret_1 = False
+Secret_2 = False
+Weapon = False
+
+# -- Levels --
+
+# Main Hall
 def intro():
   directions = ['Left', 'Forward','Right']
   print("You are stood in a large open area. It is dimly lit, with rusted walls. They are splattered in blood, the remains of your former regiment are scattered across the floor.")
   userInput = ""
   while userInput not in directions:
     print("Options: left/right/backward/forward")
-    userInput = input()
+  #userinput = input() ?
     if userInput() == "Left":
       Stairs()
     elif userInput == "Right":
       Courtyard()
     elif userInput == "Forward":
-      enemyRoom()
+      Enemy_room()
     elif userInput == "Backward":
       print("You can't chicken out now, Marine.")
     else :
-      print("Please enter a valid direction")
+      print(validOption())
+
+#Stairs
+def Stairs():
+  directions = ['Forward', 'Backward', 'Right']
+  print("You are stood at the foot of a large staircase. At the top you see a window. Beyond it, there is nothing but the harsh red wasteland of Mars. You wonder how on earth you're ever going to get home from this hell.")
+  userInput = ""
+  while userInput not in directions: 
+    print("Options: right/backward/forward")
+    if userInput == "Right":
+      Secret_A ()
+    elif userInput == "Backward":
+      print("You are travelling towards the Main Hall")
+      intro()
+    elif userInput == "Forward":
+      Weapon_room()
+    else :
+      print(validOption())  
+
+#Secret A
+def Secret_A():
+  directions = ['Backward']
+  global Secret_1
+  print("You find a skull shaped switch to the side of the stairs")
+  print("You push the switch, its eyes glow red and a section of the wall lifts up")
+  print("Infront of you there is a box of shotgun cartridges")
+  print("You place them in your pocket and suddenly, Mars doesn't seem like such a bad place after all...")
+  userInput = ""
+  while userInput not in directions:
+    print("Options: backward")
+    if userInput == "Backward":
+      print("You step out and the skull switch returns to its normal state")
+      Stairs()
+  else:
+    print(validOption())
+
+#Weapon Room
+def Weapon_room():
+  directions = ['Backward']
+  global Weapon
+  print("You climb the stairs to find a mezanine, in the center there is a platform. Floating in the middle is a sight that brings a tear to your eye.")
+  print("Glistening in the red glow of the Martian landscape is a 12 bore double-barrelled shotgun.")
+  print("You pick up the shotgun and for reasons unknown to man or God, a sinister grin creeps across your face")
+  print("You close your eyes for a moment and hear the words 'Rip and Tear' echo through your mind")
+  print("'What a time to be alive' you think to yourself as you exhale and load the shotgun 'malisciously'")
+  userInput = ""
+  while userInput not in directions: 
+    print("Options: backward")
+    if userInput == "Backward":
+      print("You walk down the stairs and are now at the foot of the stairs. Time to head into the base...")
+      Stairs()
+  else: 
+    print(validOption())
+
+#Enemy Room
+def Enemy_room():
+  directions = ['Forward', 'Backward', 'Right']
+  print("You walk up to a giant steel door with a red button in the middle")
+  print("You push the button and the whole door raises with a 'whoosh' sound")
+  print("You are stood in a corridoor that snakes around to the right, beyond the doors to your right and infront of you, you can hear all manner of demons awaiting your arrival")
+  userInput = ""
+  while userInput not in directions:
+    print("Options: forward/right/backward")
+    if userInput == "Backward":
+      print("You turn around and go back through the giant steel door. You are now travelling towards the Main Hall")
+      intro()
+    elif userInput == "Forward":
+      print("You walk towards the door infront of you. You can hear footsteps beyond the door. They sound like marching boots on a metal floor.")
+      print("Could it be your team? Are they still alive?")
+      Soldier_room()
+    elif userInput == "Right":
+      print("You walk towards another giant steel door on your right. You go to press the button to open it, but as you do you hear the dintinct sound of flesh being tore from bone")
+      print("A snarl is the only clue of what awaits behind the door. But whatever it is, it doesnt sound like anything born of Earth...")
+      Imp_room()
+  else :
+    print(validOption())
+
+#Soldier Room
+def Soldier_room():
+  directions =['Fight', 'Run']
+  print("You open the door to find a room with 2 marines inside. However, something isnt right...")
+  print("They're your former unit, but they look possesed. Their eyes red and sunken in, the flesh rotting from their bones, their mouths dripping with blood")
+  userInput = ""
+  while userInput not in directions:
+    print("Options: fight/run")
+    if userInput == "Fight":
+      if Weapon:
+        print("You pull the shotgun from you back, drop a cartridge in each barrel and unleash hell upon your former team-mates")
+        print("After blowing a hole clean through the first soliders chest, you reload.")
+        print("You run towards the second marine, shoving the end of the barrel in his snarling mouth. You squeeze the trigger and redecorate the east wall with whats left of his brains")
+        print("Satisfied with your kills, you head back towards the square room. Let's find out whats behind door number 2...")
+        Enemy_room()
+      else :
+        print("You rookie. You went and brought a knife to a gun fight.")
+        print("The soldiers charge at you and begin dismembering you whilst you're still alive...")
+        print("It's dangerous to go alone, you should have found a weapon...")
+        print("YOU HAVE DIED")
+        intro()
+    elif userInput == "Run":
+      print("You know when you're out-gunned. Maybe you should arm yourself before taking these guys on.")
+      Enemy_room()
+    else :
+      print(validOption())
+
+#Imp Room
+def Imp_room():
+  directions = ['Forward', 'Backward', 'Right', 'Left']
+  print()
+
+#Secret B
+def secret_B():
+  directions = ['Backward']
+  global Secret_2
+
+   
 
 
 
 
 
-
-
+# Intro Scene
 if __name__ == "__main__":
   while True:
     print("Welcome to MooD")
@@ -40,9 +160,6 @@ if __name__ == "__main__":
     print("For a truly immersive experience please follow this link 'https://www.youtube.com/watch?v=MEYxYcLi1lc' ")
     intro()
 
-    #Intro Scene
 
-#Stairs Scene
-  def stairs():
-    directions = ['Forward', 'Backward', 'Right']
-    #error
+
+
