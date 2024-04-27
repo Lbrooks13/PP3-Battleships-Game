fc8a08fa6dbffd7dedf7ceedcdb76d0f7865e19d
Lbrooks13 <lbrooks1308@gmail.com>
2024-04-27 15:08:50 +0100

    Timing altered to terminal output

--- run.py
+++ run.py
@@ -31,22 +31,22 @@ def intro():
   clear_terminal()
   directions = ['Left', 'Forward','Right']
   print("You are stood in a large open area.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("It is dimly lit, with rusted walls.") 
-  time.sleep(0.2)
+  time.sleep(1)
   print(
     "They are splattered in blood, "
     "the remains of your former regiment are scattered across the floor."
   )
-  time.sleep(0.2)
+  time.sleep(1)
   print("To your left you see a large staircase.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("To your right you see a window over-looking a courtyard.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Behind you is the door you used to enter the facility.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Infront of you is a giant door made of steel.")
-  time.sleep(0.2)
+  time.sleep(1)
   user_input = ""
   while user_input.capitalize() not in directions:
     print("Options: left/right/backward/forward")
@@ -72,13 +72,13 @@ def stairs():
     "You are stood at the foot of a large staircase. "
     "At the top you see a window. Beyond it, "
     "there is nothing but the harsh red wasteland of Mars.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("You wonder how on earth you're ever going to get home from this hell.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("To your right there is hidden alcove next to the stairs.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Behind you is the door back to the main hall.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Infront of you is the staircase.")
   user_input = ""
   while user_input.capitalize() not in directions: 
@@ -104,12 +104,12 @@ def secret_A():
   global SECRET_1
   SECRET_1 = True
   print("You find a skull shaped switch to the side of the stairs")
-  time.sleep(0.2)
+  time.sleep(1)
   print("You push the switch, "
         "its eyes glow red and a section of the wall lifts up")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Infront of you there is a box of shotgun cartridges")
-  time.sleep(0.2)
+  time.sleep(1)
   print("You place them in your pocket and suddenly, "
         "Mars doesn't seem like such a bad place after all...")
   user_input = ""
@@ -134,19 +134,19 @@ def weapon_room():
   print("You climb the stairs to find a mezanine, "
         "in the center there is a platform. "
         "Floating in the middle is a sight that brings a tear to your eye.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Glistening in the red glow of the Martian landscape, "
         "you see a 12 bore double-barrelled shotgun.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("You pick up the shotgun and for reasons unknown to man or God, "
         "a sinister grin creeps across your face")
-  time.sleep(0.2)
+  time.sleep(1)
   print("You close your eyes for a moment, "
         "and hear the words 'Rip and Tear' echo through your mind")
-  time.sleep(0.2)
+  time.sleep(1)
   print("'What a time to be alive' you think to yourself, "
         "as you exhale and load the shotgun 'malisciously'")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Behind you is the staircase.")
   user_input = ""
   while user_input.capitalize() not in directions: 
@@ -167,17 +167,17 @@ def enemy_room():
   clear_terminal()
   directions = ['Forward', 'Backward', 'Right']
   print("You walk up to a giant steel door with a red button in the middle")
-  time.sleep(0.2)
+  time.sleep(1)
   print("You push the button and the whole door raises with a 'whoosh' sound")
-  time.sleep(0.2)
+  time.sleep(1)
   print("You are stood in a corridoor that snakes around to the right, "
         "beyond the doors to your right and infront of you, "
         "you can hear all manner of demons awaiting your arrival")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Infront of you is a door.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("To your right is a corridor leading towards another giant steel door.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Behind you is the giant steel door heading back to the main hall.")
   user_input = ""
   while user_input.capitalize() not in directions:
@@ -192,7 +192,7 @@ def enemy_room():
       print("You walk towards the door infront of you. "
             "You can hear footsteps beyond the door. "
             "They sound like marching boots on a metal floor.")
-      time.sleep(0.2)
+      time.sleep(1)
       print("Could it be your team? Are they still alive?")
       input(continue_game)
       soldier_room()
@@ -200,7 +200,7 @@ def enemy_room():
       print("You walk towards another giant steel door on your right. "
             "You go to press the button to open it, "
             "but as you do you hear the sound of flesh being tore from bone")
-      time.sleep(0.2)
+      time.sleep(1)
       print("A snarl is the only clue of what awaits behind the door. "
             "But whatever it is, it doesnt sound like anything born of Earth...")
       input(continue_game)
@@ -217,7 +217,7 @@ def soldier_room():
   directions =['Fight', 'Run']
   print("You open the door to find a room with 2 marines inside. "
         "However, something isnt right...")
-  time.sleep(0.2)
+  time.sleep(1)
   print("They're your former unit, but they look possesed. "
         "Their eyes red and sunken in, the flesh rotting from their bones, "
         "their mouths dripping with blood")
@@ -230,27 +230,27 @@ def soldier_room():
         print("You pull the shotgun from you back, "
               "drop a cartridge in each barrel, "
               "and unleash hell upon your former team-mates")
-        time.sleep(0.2)
+        time.sleep(1)
         print("After blowing a hole clean through the first soliders chest, "
               "you reload.")
-        time.sleep(0.2)
+        time.sleep(1)
         print("You run towards the second marine, "
               "shoving the end of the barrel in his snarling mouth. "
               "You squeeze the trigger and redecorate the east wall "
               "with whats left of his brains")
-        time.sleep(0.2)
+        time.sleep(1)
         print("Satisfied with your kills, you head back towards the square "
               "room. Let's find out whats behind door number 2...")
         input(continue_game)
         enemy_room()
       else :
         print("You rookie. You went and brought a knife to a gun fight.")
-        time.sleep(0.2)
+        time.sleep(1)
         print("The soldiers charge at you and begin dismembering you whilst "
               "you're still alive...")
-        time.sleep(0.2)
+        time.sleep(1)
         print("It's dangerous to go alone, you should have found a weapon...")
-        time.sleep(0.2)
+        time.sleep(1)
         print("YOU HAVE DIED")
         input(continue_game)
         start()
@@ -269,19 +269,19 @@ def imp_room():
   clear_terminal()
   directions = ['Forward', 'Backward', 'Right', 'Left']
   print("The giant door raises up. You are now stood in a tall room.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Above you there is a window overlooking the area. "
         "You can just make-out something wandering aimlessly through the room")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Is it human?")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Infront of you is a small door with a yellow sign on the door. "
         "'BIOLOGICAL HAZZARD : BEWARE'.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("To your left is a skull shaped switch on the door.")
-  time.sleep(0.2)
+  time.sleep(1)
   print ("To your right is a small door. A red exit sign is hung above.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Behind you is the door back to the previous room.")
   user_input = ""
   while user_input.capitalize() not in directions:
@@ -318,13 +318,13 @@ def secret_B():
   SECRET_2 = True
   print("You press the skull and it's eyes glow red. "
         "You a hear a 'THUNK' from behind the switch.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("The area of wall to your left raises up revealing a secret area.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Inside you find a Medikit.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("The wall closes and the skull reverts back to normal.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("Options: backward")
   user_input = ""
   while user_input.capitalize() not in directions:
@@ -342,14 +342,14 @@ def secret_B():
 def poison_room():
   clear_terminal()
   print("The door opens to reveal a pitch black room.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("You step out and fall into a poison filled void.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("You look up in desperation but see no way out. "
         "The fumes and toxins envelop you.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("You let out a final gasp as death takes its hold on you.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("YOU HAVE DIED")
   input(continue_game)
   start()
@@ -361,7 +361,7 @@ def courtyard():
   clear_terminal()
   print("You gaze out through the window to an open area. "
         "The red sun beaming down onto the harsh Martian landscape.")
-  time.sleep(0.2)
+  time.sleep(1)
   print("The whole area is filled with demons... "
         "Maybe you'd better sit this one out until you can find more "
         "substantial weaponry.")
